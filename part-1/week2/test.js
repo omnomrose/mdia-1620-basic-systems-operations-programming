443e809e439043f08b4293f6b96f37381e0a09ba
Rose Nguyen <mnguyen187@se14l0120-21.ad.bcit.ca>
2022-09-16 15:57:23 -0700

    meow #2

--- part-1/week2/test.js
+++ part-1/week2/test.js
@@ -8,4 +8,57 @@ var electronicDevices;
 var ElectronicDevices;
 
 // snake case
-var Electronic_Devices
+var Electronic_Devices;
+
+// Initialise the variable age to the value to 14
+// Use camelCase
+
+var age = 14;
+
+console.log(age);
+
+// String
+var name = "rose";
+
+// Boolean
+// Initialise the variable called proceed to the value true.
+var proceed = true;
+var i = 0;
+
+while (proceed) {
+    i++;
+    proceed = false;
+}
+
+console.log(i);
+
+// Objects
+/**
+ * 6-4-1 Create a variable called grannySmith and add the following values (use camelCase):
+* colour of the apple is green
+* origin location is australia
+* origin date is 1868
+* Then console log out the colour from the object.
+ */
+
+var grannySmith = {
+    color: "green",
+    originLocation: "australia",
+    originDate: 1868
+};
+
+console.log(grannySmith);
+
+console.log(grannySmith.color);
+
+// 6-5-1 Initialise the variable x to a function called 
+// myFunction with the values 1 and 3.
+// multiply the two numbers. Console log the variable x afterwards
+
+var x = myFunction (1,3);
+
+function myFunction (numOne, numTwo) {
+    return numOne * numTwo;
+};
+
+console.log(x);
