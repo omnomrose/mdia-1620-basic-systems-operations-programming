cd29425459fc97ae604b69bda575eeded1827059
MaryRose Nguyen <rose@MaryRoses-MacBook-Pro.local>
2022-09-25 00:24:11 -0700

    adding new files and folders

---
+++ part-1/Week 3/string-formatting/example.js
@@ -0,0 +1,12 @@
+//backticks
+var age = 40;
+
+var sentence = `George is ${age} years old.`;
+
+console.log(sentence);
+
+//plus operator
+
+var sentenceTwo = "George is " + age + " years old.";
+
+console.log (sentenceTwo);
