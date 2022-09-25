6a0f0d1d37e6a43064a998b323fc11a89b99ddb8
MaryRose Nguyen <rose@MaryRoses-MacBook-Pro.local>
2022-09-25 00:36:38 -0700

    new code

---
+++ part-1/Week 3/test.js
@@ -0,0 +1,15 @@
+// This is a single line comment
+
+/**
+ * This is a description
+ * Hello hi!
+ * Meow mew
+ */
+
+var x = myFunction (2,3);
+
+function myFunction(numOne, numTwo) {
+    return numOne * numTwo;
+}
+
+// adding new line
